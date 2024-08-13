ef33da72827cc186b393fbef26d73f99216cf5a7
EyalAv1 <eyalavni6@gmail.com>
2024-08-13 18:17:37 +0300

    13-08-24 commit

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -1,35 +1,35 @@
-import classes from './HomePage.module.css';
+import './HomePage.css';
 import { Link } from 'react-router-dom';
 
 function HomePage() {
     return (
         <>
-            <div className={classes.cintainer}>
-                <div className={classes.container_content}>
-                    <div className={classes.container_content_p}>
+            <div className="cintainer">
+                <div className="container_content">
+                    <div className="container_content_p">
                         <h1>Lets Build Your Path To Healthy Life</h1>
                         <Link to='/PersonalMenu'>
-                            <div className={classes.text}>
+                            <div className="text">
                                 <span>🔥click here to start your process🔥</span>
                             </div>
                         </Link>
                     </div>
                 </div>
             </div>
-            <div className={classes.about_me_outline}>
-                <div className={classes.about_me_container}>
-                    <div className={classes.about_me_container_content}>
+            <div className="about_me_outline">
+                <div className="about_me_container">
+                    <div className="about_me_container_content">
                         <h1>About Me</h1>
                     </div>
                 </div>
                 <div>
-                <p>
-                    All my life I tried to handle my meals! So I decided to use my development skills to make a free planner for myself. <br/>
-                    Here you can build your menu and count your calories ✅ <br />
-                    Now, I have chosen to share with you a product I developed! <br />
-                    Have fun 🔥💪🏽
-                </p>
-                </div>    
+                    <p>
+                        All my life I tried to handle my meals! So I decided to use my development skills to make a free planner for myself. <br />
+                        Here you can build your menu and count your calories ✅ <br />
+                        Now, I have chosen to share with you a product I developed! <br />
+                        Have fun 🔥💪🏽
+                    </p>
+                </div>
             </div>
         </>
     );
